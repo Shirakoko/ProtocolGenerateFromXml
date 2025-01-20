deb7a5fdca5af703a27077f46146bc508b0bc8d3
matsuki-aFHZE <1971173893@qq.com>
2025-01-20 18:42:04 +0800

    第一版

---
+++ Editor/ProtocolGenerate.cs
@@ -0,0 +1,35 @@
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+
+public class ProtocolGenerate
+{
+    // 配置文件所在路径
+    private static string PROTO_INFO_PATH = Application.dataPath + "/Editor/ProtocolGenerate/Test.xml";
+    private static GenerateCsharpProtocol _generator = new GenerateCsharpProtocol();
+
+    [MenuItem("ProtolGenerate/生成C#脚本")]
+    private static void GenerateCSharp()
+    {
+        Debug.Log("生成C#代码");
+        XmlNode root = GetRoot("messages");
+        _generator.GenerateEnum(GetChildrenNodes(root, "enum"));
+        _generator.GenerateData(GetChildrenNodes(root, "data"));
+
+        AssetDatabase.Refresh(); // 刷新Assets文件夹
+    }
+
+    private static XmlNode GetRoot(string rootName)
+    {
+        XmlDocument xml = new XmlDocument();
+        xml.Load(PROTO_INFO_PATH);
+        XmlNode root = xml.SelectSingleNode(rootName);
+
+        return root;
+    }
+
+    private static XmlNodeList GetChildrenNodes(XmlNode node, string nodeName)
+    {
+        return node.SelectNodes(nodeName);
+    }
+}
